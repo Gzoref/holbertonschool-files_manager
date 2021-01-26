0d79c2178dc496088c5032f2118c31f21da89c80
Gzoref <gzoref@gmail.com>
2021-01-25 21:02:30 -0500

    Add client to constructor

--- utils/redis.js
+++ utils/redis.js
@@ -1,8 +1,16 @@
 import redis from "redis";
+import { promisify } from "util";
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+
+    this.client.on("error", (error) => {
+      console.log(error);
+    });
+
+    this.client.on("connect", () => {});
   }
 
   isAlive() {
