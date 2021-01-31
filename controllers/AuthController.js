4aa26501a7d55ea3482dbf4ea8476150ec3902c6
jfkquality <jfkquality@gmail.com>
2021-01-31 15:16:48 -0800

    Task 4. [bugfix]. Use try, catch everywhere to cCatch bad pw, base64.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,36 +7,47 @@ class AuthController {
   static async getConnect(request, response) {
     const authHeader = request.headers.authorization;
     if (!authHeader) {
-      response.status(401).json({ error: 'Unauthorized' });
+      response.status(401).json({ error: 'Unauthorized AUTH 1' });
     }
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    const email = auth[0];
-    const pass = sha1(auth[1]);
+    try {
+      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+      const email = auth[0];
+      const pass = sha1(auth[1]);
 
-    const user = await dbClient.getUser({ email });
+      const user = await dbClient.getUser({ email });
+      console.log('USER IN AUTH GETCONNECT()', user);
 
-    if (!user || pass !== user.password) {
-      response.status(401).json({ error: 'Unauthorized' });
+      if (!user || pass !== user.password) {
+        response.status(401).json({ error: 'Unauthorized AUTH 2' });
+      }
+
+      const token = uuidv4();
+      const key = `auth_${token}`;
+      const duration = (60 * 60 * 24);
+      await redisClient.set(key, user._id.toString(), duration);
+
+      response.status(200).json({ token });
+    } catch (err) {
+      console.log(err);
+      response.status(500).json({ error: 'Server error' });
     }
-
-    const token = uuidv4();
-    const key = `auth_${token}`;
-    const duration = (60 * 60 * 24);
-    await redisClient.set(key, user._id.toString(), duration);
-
-    response.status(200).json({ token });
   }
 
   static async getDisconnect(request, response) {
-    const userToken = request.header('X-Token');
-    // console.log('USER TOKEN DISCONNECT', userToken);
-    const userKey = await redisClient.get(`auth_${userToken}`);
-    // console.log('USER KEY DISCONNECT', userKey);
-    if (!userKey) {
-      response.status(401).json({ error: 'Unauthorized' });
+    try {
+      const userToken = request.header('X-Token');
+      // console.log('USER TOKEN DISCONNECT', userToken);
+      const userKey = await redisClient.get(`auth_${userToken}`);
+      // console.log('USER KEY DISCONNECT', userKey);
+      if (!userKey) {
+        response.status(401).json({ error: 'Unauthorized AUTH 3' });
+      }
+      await redisClient.del(`auth_${userToken}`);
+      response.status(204).send('DISCONNECTED');
+    } catch (err) {
+      console.log(err);
+      response.status(500).json({ error: 'Server error' });
     }
-    await redisClient.del(`auth_${userToken}`);
-    response.status(204).send('DISCONNECTED');
   }
 }
 
